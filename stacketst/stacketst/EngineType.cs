29365e9a9ebd8ed742465729623123d7ee232a89
bhargavak <bhargavak37@gmail.com>
2017-08-20 11:14:27 +0530

    Stack Test in CSharp
    Stack Test in CSharp

---
+++ stacketst/stacketst/EngineType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace stacketst
+{
+    public class EngineType
+    {
+        public enum name_engines
+        {
+
+            V12, V10, V8, V6, UZ
+
+        };
+    }
+}
---
+++ stacketst/stacketst/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stacketst
+{
+
+    public class CarsBase
+    {
+        public string DisplayName { get; set; }
+        public CarsBase()
+        {
+            DisplayName = "Base Car";
+
+        }
+    }
+    public class Toyota : CarsBase
+    {
+       
+        public int number_of_wheels { get; set; }
+        public double fuel_capacity { get; set; }
+        public string engine_type { get; set; }
+
+        public Toyota()
+        {
+            fuel_capacity = 4.2;
+        number_of_wheels = 4;
+  engine_type = EngineType.name_engines.UZ.ToString();
+        }
+ 
+    }
+
+    public class TestClass
+    {
+        
+        static void Main()
+        {
+            var myVar = new Toyota();
+            Console.WriteLine(myVar.DisplayName);
+            Console.ReadLine();     
+    }
+}
+  
+}
+
